2e8f3ad63886bcb7d2149579efdd45f697c42aa3
Noe Davenne <ndavenne@c1r1p5.42lehavre.fr>
2024-03-11 12:39:00 +0100

    norm push

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: ndavenne <ndavenne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 11:40:24 by ndavenne          #+#    #+#             */
-/*   Updated: 2024/03/11 11:48:04 by ndavenne         ###   ########.fr       */
+/*   Updated: 2024/03/11 12:38:22 by ndavenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,11 @@ int	ft_putnbr_base(long n, char *base)
 	return (_recursivite(n, base, base_size, nb_printed));
 }
 
-static int	_recursivite2(size_t l, char *base, size_t base_size, int nb_printed)
+static int	_recursivite2(size_t l, char *base, size_t base_len, int nb_printed)
 {
-	if (l >= base_size)
-		nb_printed = _recursivite(l / base_size, base, base_size, nb_printed);
-	ft_putchar(base[l % base_size]);
+	if (l >= base_len)
+		nb_printed = _recursivite(l / base_len, base, base_len, nb_printed);
+	ft_putchar(base[l % base_len]);
 	return (nb_printed + 1);
 }
 
